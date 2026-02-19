8433d36dac804f0b8eaaba509d2341ebe89a576b
Gmnaqib <gumilarmn16@gmail.com>
2026-02-19 10:21:47 +0700

    fix: update role middlewares for KPI and research routes to include lecturer role

--- src/routes/kpi.route.ts
+++ src/routes/kpi.route.ts
@@ -4,21 +4,21 @@ import { authenticateToken } from "../middlewares/auth.middlewares";
 import { roleMiddlewares } from "../middlewares/role.middlewares";
 const kpiRouter = Router();
 
-kpiRouter.get("/operational", authenticateToken, roleMiddlewares(["admin", "minister of operation"]), KPIController.getAllOperationalSummary);
+kpiRouter.get("/operational", authenticateToken, roleMiddlewares(["admin", "minister of operation", "lecturer"]), KPIController.getAllOperationalSummary);
 kpiRouter.get("/operational/me", authenticateToken, KPIController.getOperationalSummary);
-kpiRouter.get("/operational/:userId", authenticateToken, roleMiddlewares(["admin", "minister of operation"]), KPIController.getOperationalSummaryById);
+kpiRouter.get("/operational/:userId", authenticateToken, roleMiddlewares(["admin", "minister of operation", "lecturer"]), KPIController.getOperationalSummaryById);
 
-kpiRouter.get("/research", authenticateToken, roleMiddlewares(["admin", "minister of research"]), KPIController.getAllResearchSummary);
+kpiRouter.get("/research", authenticateToken, roleMiddlewares(["admin", "minister of research", "lecturer"]), KPIController.getAllResearchSummary);
 kpiRouter.get("/research/me", authenticateToken, KPIController.getResearchSummary);
-kpiRouter.get("/research/:userId", authenticateToken, roleMiddlewares(["admin", "minister of research"]), KPIController.getResearchSummaryById);
+kpiRouter.get("/research/:userId", authenticateToken, roleMiddlewares(["admin", "minister of research", "lecturer"]), KPIController.getResearchSummaryById);
 
-kpiRouter.get("/branding", authenticateToken, roleMiddlewares(["admin", "minister of branding"]), KPIController.getAllBrandingSummary);
+kpiRouter.get("/branding", authenticateToken, roleMiddlewares(["admin", "minister of branding", "lecturer"]), KPIController.getAllBrandingSummary);
 kpiRouter.get("/branding/me", authenticateToken, KPIController.getBrandingSummary);
-kpiRouter.get("/branding/:userId", authenticateToken, roleMiddlewares(["admin", "minister of branding"]), KPIController.getBrandingSummaryById);
+kpiRouter.get("/branding/:userId", authenticateToken, roleMiddlewares(["admin", "minister of branding", "lecturer"]), KPIController.getBrandingSummaryById);
 
-kpiRouter.get("/competition", authenticateToken, roleMiddlewares(["admin", "minister of competition"]), KPIController.getAllCompetitionSummary);
+kpiRouter.get("/competition", authenticateToken, roleMiddlewares(["admin", "minister of competition", "lecturer"]), KPIController.getAllCompetitionSummary);
 kpiRouter.get("/competition/me", authenticateToken, KPIController.getCompetitionSummary);
-kpiRouter.get("/competition/:userId", authenticateToken, roleMiddlewares(["admin", "minister of competition"]), KPIController.getCompetitionSummaryById);
+kpiRouter.get("/competition/:userId", authenticateToken, roleMiddlewares(["admin", "minister of competition", "lecturer"]), KPIController.getCompetitionSummaryById);
 
 kpiRouter.get("/", authenticateToken, KPIController.getAllTotalPointSummary);
 kpiRouter.get("/statistic", authenticateToken, KPIController.kpiStatistic);
--- src/routes/research.route.ts
+++ src/routes/research.route.ts
@@ -8,7 +8,7 @@ const researchRouter = Router();
 researchRouter.get("/me", authenticateToken, researchController.getMyResearch);
 researchRouter.patch("/me/:id", authenticateToken, researchController.updateMyResearch);
 researchRouter.post("/", authenticateToken, researchController.createResearch);
-researchRouter.delete("/:id", authenticateToken, roleMiddlewares(["admin", "minister of research"]), researchController.deleteResearch);
+researchRouter.delete("/:id", authenticateToken, roleMiddlewares(["admin", "minister of research", "president", "lecturer"]), researchController.deleteResearch);
 researchRouter.get("/", authenticateToken, roleMiddlewares(["admin", "minister of research", "lecturer"]), researchController.getAllResearch);
 researchRouter.get("/:id", authenticateToken, roleMiddlewares(["admin", "minister of research", "lecturer"]), researchController.getResearchById);
 researchRouter.patch("/:id", authenticateToken, roleMiddlewares(["admin", "minister of research", "lecturer"]), researchController.updateResearch);
